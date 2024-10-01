3dc3b1671216438472580c8948f4e5733ebb78a1
CaoAnhVu141 <caovu5541@gmail.com>
2024-10-01 13:01:02 +0700

    commit by caoanhvu

---
+++ new_security/configs/database.php
@@ -0,0 +1,12 @@
+<?php
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_PORT', 4433);
+// define('DB_NAME', 'app_web1');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'app_demo');
+define('DB_PORT', 4433);
+
